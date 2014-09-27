be0d402fb901dad4325b99edf7eb6412ebb547b6
Jakub Adamczyk <andrzej@golonka.pl>
2014-09-27 16:03:20 +0200

    big big commit

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,14 +1,14 @@
 class Notifications < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "app30084236@heroku.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.notifications.hello.subject
   #
-  def hello
-    @greeting = "Hi"
-
-    mail to: "jakub.spike@gmail.com"
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
   end
 end
